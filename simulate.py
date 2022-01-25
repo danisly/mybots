caa00f3d411bb61ee9ae09bc96da5914061e4bdb
dly1 <dly1@uvm.edu>
2022-01-25 14:07:21 -0500

    Array of towers

--- simulate.py
+++ simulate.py
@@ -1,3 +1,15 @@
 import pybullet as p
+import time
+import pybullet_data
+
 physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+p.setGravity(0,0,-9.8)
+planeId = p.loadURDF("plane.urdf")
+p.loadSDF("boxes.sdf")
+for i in range(0,1000):
+    p.stepSimulation()
+    print(i)
+    time.sleep(.02)
 p.disconnect()
+
