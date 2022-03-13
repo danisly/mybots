cdd544143fa021640b4d7016b87111cccf7329b5
dly1 <dly1@uvm.edu>
2022-03-13 13:56:03 -0400

    simulation

---
+++ simulation.py
@@ -0,0 +1,30 @@
+import pybullet as p
+import pybullet_data
+import pyrosim.pyrosim as pyrosim
+import time
+from world import WORLD
+from robot import ROBOT
+
+class SIMULATION:
+    def __init__(self):
+        self.physicsClient = p.connect(p.GUI)
+        p.setAdditionalSearchPath(pybullet_data.getDataPath())
+        p.setGravity(0,0,-9.8)
+
+        self.world = WORLD()
+        self.robot = ROBOT()
+
+    def Run(self):
+        for t in range(0,1000):
+            p.stepSimulation()
+            self.robot.Sense(t)
+            self.robot.Think()
+            self.robot.Act(t)
+            time.sleep(1/240)
+##            print(backLegSensorValues)
+##
+##
+
+    
+    def __del__(self):
+        p.disconnect()
