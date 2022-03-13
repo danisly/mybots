394f760908eb9e6a51ba88a29f92c9c1dcaf2ca0
dly1 <dly1@uvm.edu>
2022-03-13 13:55:30 -0400

    sensors

---
+++ sensor.py
@@ -0,0 +1,17 @@
+import numpy as numpy
+import pyrosim.pyrosim as pyrosim
+
+class SENSOR:
+    
+    def __init__(self, linkName):
+        self.linkName = linkName
+        self.values = numpy.zeros(1000)
+
+    def Get_Value(self, t):
+        self.values[t] = pyrosim.Get_Touch_Sensor_Value_For_Link(self.linkName)
+
+    def Save_Values(self):
+        
+        numpy.save(r"C:\Users\hikar\mybots\data\\sensorValues.npy", self.values)
+
+
